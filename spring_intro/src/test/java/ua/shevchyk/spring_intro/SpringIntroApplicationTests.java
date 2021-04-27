60c53eb7646382913229ceb45bd233955b3843aa
Shevchyk-src <illia.shevchyk@ukma.edu.ua>
2021-04-27 17:23:13 +0300

    Commit introduction to spring

---
+++ spring_intro/src/test/java/ua/shevchyk/spring_intro/SpringIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.shevchyk.spring_intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringIntroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
